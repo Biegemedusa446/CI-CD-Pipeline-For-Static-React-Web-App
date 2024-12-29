e9a16c8e42ee7f5b87fd43b83adf99dfc63cbbb3
Laghari <m.naeemlaghari@gmail.com>
2024-12-29 20:54:07 +0100

    Switching between environments

--- my-react-app/src/config.js
+++ my-react-app/src/config.js
@@ -1,16 +1,2 @@
-const ENV = process.env.REACT_APP_ENV || 'production';
-
-const configs = {
-  production: {
-    environment: 'production',
-    featureFlag: false,
-  },
-  staging: {
-    environment: 'staging',
-    featureFlag: true,
-
-  },
-};
-
 const config = 'production';
 export default config;
