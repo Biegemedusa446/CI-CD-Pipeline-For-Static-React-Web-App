ba58aec7e5bcf8c3d8169924db44492337d3b2be
Laghari <m.naeemlaghari@gmail.com>
2024-12-29 21:08:36 +0100

    Switching between environments

--- my-react-app/src/App.js
+++ my-react-app/src/App.js
@@ -1,10 +1,8 @@
-import config from './config';
-
 function App() {
   return (
     <div>
-      <h1>Welcome to the {config.environment.toUpperCase()} Environment</h1>
-      {config.featureFlag && <p>Experimental features are enabled!</p>}
+      <h1>Welcome to the Production</h1>
+      <p>Experimental features are enabled!</p>
     </div>
   );
 }
