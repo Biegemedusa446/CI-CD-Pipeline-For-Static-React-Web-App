a4ab9a7902adbb7471598d4e4d9ab6b57ee24feb
Laghari <m.naeemlaghari@gmail.com>
2025-01-15 20:41:23 +0100

    Minor Changes

--- my-react-app/src/App.js
+++ my-react-app/src/App.js
@@ -3,7 +3,7 @@ function App() {
     <div>
       <h1>Welcome to the Production</h1>
       <p>Experimental features are enabled!</p>
-      <p>Have a nice day</p>
+      <p>Have a nice day!!</p>
     </div>
   );
 }
